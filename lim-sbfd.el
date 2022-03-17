7d8dc9411836f0cc0260014146f70862e7c14a1a
Gaeric <1062217098@qq.com>
2022-03-17 20:21:44 +0800

    refactor lim-handle-sbfd

--- lim-sbfd.el
+++ lim-sbfd.el
@@ -9,30 +9,32 @@
 (require 'lim-core)
 (require 'lim-advanced)
 
-(defun lim-handle-string-sbfd ()
-  (if (if (> (length lim-current-string) 2)
-          (not (member (char-to-string last-command-event) lim-possible-char))
-        (> (length lim-current-string) 4))
-      (let ((candidates (cdr (car lim-optional-result)))
-            current-item)
-        (setq current-item
-              (cond
-               ((equal ?a last-command-event) (nth 1 candidates))
-               ((equal ?e last-command-event) (nth 2 candidates))
-               ((equal ?u last-command-event) (nth 3 candidates))
-               ((equal ?i last-command-event) (nth 4 candidates))
-               ((equal ?o last-command-event) (nth 5 candidates))
-               (t nil)))
+(defun lim-handle-sbfd ()
+  "sbfd handle current string"
+  (let ((re (lim-get lim-current-string))
+        (candidates (cdr car lim-optional-result))
+        current-item)
+    (if (caar re)
+        (progn
+          (setq 
+           lim-current-word (caar re)
+           lim-possible-char (cdr (assoc "completions" re))
+           lim-current-pos 1
+           lim-optional-result re)
+          (if (functionp 'lim-show) (funcall 'lim-show)))
+      (setq current-item
+            (cond
+             ((equal ?a last-command-event) (nth 1 candidates))
+             ((equal ?e last-command-event) (nth 2 candidates))
+             ((equal ?u last-command-event) (nth 3 candidates))
+             ((equal ?i last-command-event) (nth 4 candidates))
+             ((equal ?o last-command-event) (nth 5 candidates))
+             (t nil)))
         (lim-terminate-translation)
         (unless current-item
           (setq unread-command-events (cons last-command-event unread-command-events))
           (setq current-item (car candidates)))
-        (setq lim-current-word (car current-item)))
-    (setq lim-optional-result (lim-get lim-current-string)
-          lim-current-word (car (car lim-optional-result))
-          lim-possible-char (cdr (assoc "completions" lim-optional-result))
-          lim-current-pos 1)
-    (if (functionp 'lim-show) (funcall 'lim-show))))
+        (setq lim-current-word (car current-item)))))
 
 (autoload 'lim-use-package "lim-xixi" "Lightly input mehtod xixi")
 (autoload 'lim-orderless-regexp "lim-tools")
@@ -50,7 +52,7 @@
 (defun lim-load-sbfd ()
   (setq lim-punctuation-list (lim-read-punctuation lim-current-scheme))
   (setq lim-translate-function 'lim-punctuation-translate)
-  (setq lim-handle-function 'lim-handle-string-sbfd)
+  (setq lim-handle-function 'lim-handle-sbfd)
   (with-eval-after-load 'evil (lim-evil-find-mode)))
 
 (defvar lim-ascii-char (cons ?\' "‘’")
